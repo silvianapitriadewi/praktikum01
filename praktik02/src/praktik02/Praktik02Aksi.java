9234db89180be9ebdfc633c60dba1a7b321f753f
silvianapitriadewi <silvianadewi410@gmail.com>
2018-04-05 22:24:22 -0700

    Mengisi Nilai Atribut

--- praktik02/src/praktik02/Praktik02Aksi.java
+++ praktik02/src/praktik02/Praktik02Aksi.java
@@ -4,5 +4,18 @@ package praktik02;
 public class Praktik02Aksi {
     public static void main(String[] args) {
         Praktik02 data_penduduk = new Praktik02();
+        
+       data_penduduk.nama="Dewi";
+       data_penduduk.agama="Islam";
+       data_penduduk.alamat="Komp.Wengga kuda";
+       data_penduduk.gd="B";
+       data_penduduk.jk="Wanita";
+       data_penduduk.nik="6301055811960001";
+       data_penduduk.profesi="mahasiswa";
+       data_penduduk.status="Belum Kawin";
+       data_penduduk.tgl_lahir="18-11-1996";
+       data_penduduk.tl="Banjarbaru";
+       
+       
     }
 }
