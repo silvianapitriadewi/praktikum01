21949ac894c15abfcfbd0f676b559feb80de46b0
silvianapitriadewi <silvianadewi410@gmail.com>
2018-04-05 22:01:29 -0700

    Membuat Atribut dan behavior

--- praktik02/src/praktik02/Praktik02.java
+++ praktik02/src/praktik02/Praktik02.java
@@ -3,6 +3,20 @@ package praktik02;
 
 
 public class Praktik02 {
+    double nik,tgl_lahir;
+   String nama,alamat,tl,jk,agama,gd,profesi,status;
    
+   void cetakhasil(){
+       System.out.println("NIK  :"+nik);
+       System.out.println("Nama  :"+nama);
+       System.out.println("Tanggal Lahir  :"+tgl_lahir);
+       System.out.println("Tempat Lahir  :"+tl);
+       System.out.println("Jenis Kelamin  :"+jk);
+       System.out.println("Agama  :"+agama);
+       System.out.println("Gol. Darah  :"+gd);
+       System.out.println("profesi  :"+profesi);
+       System.out.println("status  :"+status);
+       System.out.println("alamat  :"+alamat);
+   }
     
 }
